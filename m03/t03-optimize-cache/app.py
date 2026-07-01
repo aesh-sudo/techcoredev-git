30550a5786c955a5d0a77151342859eb0ef2dbef
aesh <anton.shelashsky@techcoredev.ru>
2026-07-01 12:50:41 +0000

    refactor(m03-t03): optimize dockerfile cache

--- m03/t03-optimize-cache/app.py
+++ m03/t03-optimize-cache/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!!! Я работаю внутри Docker-контейнера!</h1>'
+    return '<h1>Hello, World!!! Я работаю внутри Docker-контейнера!!!</h1>'
 
 if __name__ == '__main__':
     # host='0.0.0.0' открывает доступ к приложению извне контейнера
